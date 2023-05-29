87acbb3626ecdfb0aa862f1286271709b3ffc00b
BeMora11 <dronemaxis_935@outlook.com>
2023-05-29 14:25:42 -0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, createBrowserRouter } from 'react-router-dom';
 import HomePage from './app/pages/HomePage';
 
 const App = () => {
-  const router = createBrowserRouter([
-    { path: '', element: <HomePage /> }
-  ]);
 
-  return <RouterProvider router={router} />
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
