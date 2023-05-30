634e750f8fb43d6349c21c4785ccac282b8aa1c9
BeMora11 <dronemaxis_935@outlook.com>
2023-05-30 09:10:15 -0600

    firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './app/pages/HomePage';
 
 const App = () => {
